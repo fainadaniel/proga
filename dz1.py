329fd9557539d1ae01978dd6c623ffbb0cc7a74f
fainadaniel <faina.daniel@gmail.com>
2017-09-25 21:46:01 +0200

    Add files via upload

---
+++ dz1.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a * b == c:
+    print('a * b = c')
+else:
+    print('a * b != c')
+
+if a / b == c:
+    print('a / b = c')
+else:
+    print('a / b != c')
